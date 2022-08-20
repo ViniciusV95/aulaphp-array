15c2c0402b1cabcbbcaa4b1d33b74785f41accea
Vinícius Vieira <110571848+ViniciusV95@users.noreply.github.com>
2022-08-19 21:54:42 -0300

    Add files via upload
    Aula 19/08

---
+++ 19-08/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Desenvolvido por Vinicius</p>
+</footer>
+</body>
+</html>
---
+++ 19-08/sobre.php
@@ -0,0 +1,13 @@
+<?php include "header.php"; ?>
+    <main>
+        <h1> Sobre a IMDB</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi aspernatur pariatur nisi non aliquam esse dolores, recusandae nulla repellendus voluptates eligendi quaerat libero iusto saepe? Possimus cupiditate nesciunt aperiam maiores?
+        </p>    
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi aspernatur pariatur nisi non aliquam esse dolores, recusandae nulla repellendus voluptates eligendi quaerat libero iusto saepe? Possimus cupiditate nesciunt aperiam maiores?
+        </p>      
+    </main>
+
+
+<?php include "footer.php"; ?>
